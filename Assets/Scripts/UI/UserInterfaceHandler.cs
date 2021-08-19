5b16e590ea71b3d8280a9badb994d1f6e1d84240
Brandon Lacy <brandon.lacy@kiewit.com>
2021-08-19 14:12:43 -0500

    Added inventory toggling with Key

---
+++ Assets/Scripts/UI/UserInterfaceHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+public class UserInterfaceHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject inventory;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(InputManager.Instance.InventoryPanel)) 
+        {
+            inventory.SetActive(!inventory.activeSelf);
+        }
+    }
+}
